f66d5d5464e55593271e384a31e3b1e17fdec692
GNG <GNG@Guilhermes-MacBook-Pro.local>
2020-09-02 04:45:28 -0300

    TEST

--- config/server.js
+++ config/server.js
@@ -1,4 +1,4 @@
 module.exports = ({ env }) => ({
   host: env("HOST", `${process.env.HOST}` || '0.0.0.0'),
-  port: env.int('PORT', `${process.env.PORT}` || '5000')
+  port: env.int('PORT', `${process.env.PORT}` || '1337')
 });
