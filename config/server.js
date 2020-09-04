2e6b0831d53b70a4c328e22e9ee3533761c9788a
GNG <GNG@Guilhermes-MacBook-Pro.local>
2020-09-04 14:39:42 -0300

    make it better

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  // url: env("URL",'https://evaluatz-db.herokuapp.com')
+  url: env("URL",'https://evaluatz-db.herokuapp.com')
 });
