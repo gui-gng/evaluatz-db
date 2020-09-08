4afa2d07d4d1daff18395e367fef4a7c1cf00908
GNG <GNG@Guilhermes-MacBook-Pro.local>
2020-09-08 10:37:02 -0300

    update connection

--- config/database.js
+++ config/database.js
@@ -1,15 +1,33 @@
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "sqlite",
-        filename: env("DATABASE_FILENAME", ".tmp/data.db")
-      },
-      options: {
-        useNullAsDefault: true
+module.exports = ({ env }) => {
+
+
+
+  if (process.env.DATABASE_URL) {
+    const databaseConfig = process.env.DATABASE_URL.split("//")[1];
+    const [username, passwordAndDb, hostAndPort] = databaseConfig.split(":");
+    const [password, host] = passwordAndDb.split("@");
+    const [port, database] = hostAndPort.split("/");
+
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          "settings": {
+            "client": "postgres",
+            host,
+            port,
+            username,
+            password,
+            database,
+            schema: "public"
+          },
+          options: {
+            useNullAsDefault: true
+          }
+        }
       }
     }
   }
-});
+
+};
